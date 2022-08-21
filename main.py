18b6cd9bd51ef503ca2c8e0ca9874af2425b29c4
Asad <asad200ana@gmail.com>
2022-08-22 04:32:18 +0530

    sprites

--- main.py
+++ main.py
@@ -4,12 +4,7 @@ from settings import *
 from map import *
 from player import *
 from raycasting import *
-from object_renderer import *
 from sprite_object import *
-from object_handler import *
-from weapon import *
-from sound import *
-from pathfinding import *
 
 
 class Game:
@@ -27,18 +22,13 @@ class Game:
     def new_game(self):
         self.map = Map(self)
         self.player = Player(self)
-        self.object_renderer = ObjectRenderer(self)
         self.raycasting = RayCasting(self)
-        self.object_handler = ObjectHandler(self)
-        self.weapon = Weapon(self)
-        self.sound = Sound(self)
-        self.pathfinding = PathFinding(self)
-        pg.mixer.music.play(-1)
+        self.static_sprite = SpriteObject(self)
 
     def update(self):
         self.player.update()
         self.raycasting.update()
-        self.object_handler.update()
+        self.static_sprite.update()
         self.weapon.update()
         pg.display.flip()
         self.delta_time = self.clock.tick(FPS)
