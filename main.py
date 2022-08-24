9f64d8195c949c124397b5b0dc7b46a49381f792
Asad <asad200ana@gmail.com>
2022-08-24 20:17:50 +0530

    done

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ from settings import *
 from map import *
 from player import *
 from raycasting import *
+from object_renderer import *
 from sprite_object import *
+from object_handler import *
+from weapon import *
+from sound import *
+from pathfinding import *
 
 
 class Game:
@@ -22,22 +27,30 @@ class Game:
     def new_game(self):
         self.map = Map(self)
         self.player = Player(self)
+        self.object_renderer = ObjectRenderer(self)
         self.raycasting = RayCasting(self)
-        self.static_sprite = SpriteObject(self)
+        self.object_handler = ObjectHandler(self)
+        self.weapon = Weapon(self)
+        self.sound = Sound(self)
+        self.pathfinding = PathFinding(self)
+        pg.mixer.music.play(-1)
 
     def update(self):
         self.player.update()
         self.raycasting.update()
-        self.static_sprite.update()
+        self.object_handler.update()
         self.weapon.update()
         pg.display.flip()
         self.delta_time = self.clock.tick(FPS)
         pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
 
     def draw(self):
+        # self.screen.fill('black')
         self.object_renderer.draw()
         self.weapon.draw()
-       
+        # self.map.draw()
+        # self.player.draw()
+
     def check_events(self):
         self.global_trigger = False
         for event in pg.event.get():
